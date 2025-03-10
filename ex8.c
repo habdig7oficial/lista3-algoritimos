17268967db9014a76266e4480a545bf8beedf375
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-03-10 15:59:48 -0300

    progress

--- ex8.c
+++ ex8.c
@@ -1,1 +1,26 @@
-#include
+#include "stdio.h"
+
+double celsius_to_fahrenheit(double celsius){
+    return (celsius * 1.8) + 32;
+}  
+double fahrenheit_to_celsius(double celsius){
+    return (celsius * 1.8) + 32;
+}   
+
+int main(){
+
+    typedef double (*ref_func) (double);
+
+    ref_func funcs[] = {celsius_to_fahrenheit, fahrenheit_to_celsius};
+    char texts[][24] = {"Celsius para Fahrenheit", "Fahrenheit para Celsius"};
+
+    for(int i = 0; i < sizeof(texts) / sizeof(texts[0]); i++)
+        printf("%d) %s\n", i + 1, texts[i]);
+
+    int opt
+    printf(">>> %d", )
+   
+
+
+    return 0;
+}
