1043c97e7563f9bbdbf30c98c16a95683c3ee539
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-03-10 15:35:09 -0300

    ok

--- ex7.c
+++ ex7.c
@@ -1,3 +1,5 @@
+/* Ainda Dava pra melhorar */
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
@@ -7,19 +9,23 @@ struct ContaNumeros{
     int impares;
     int *arr_pares;
     int *arr_impares;
+    int distancia;
 };
 
 struct ContaNumeros conta_numeros(int inicio, int fim){
     int intervalo = fim - inicio;
 
+    printf("%d\n\n", intervalo);
+
     int med = floor(intervalo / 2);
-    int num_impares[med];
-    int num_pares[med];
+    int * num_impares = malloc(sizeof(int) * med);
+    int * num_pares = malloc(sizeof(int) * med);
     struct ContaNumeros res = {
-        .pares = med,
+        .pares = intervalo % 2 == 0 ? (med - 1) : med,
         .impares = med,
         .arr_pares = num_pares,
-        .arr_impares = num_impares
+        .arr_impares = num_impares,
+        .distancia = med
     };
 
 
@@ -38,17 +44,23 @@ struct ContaNumeros conta_numeros(int inicio, int fim){
         }
     }
 
-    printf("%d\n\n",res.arr_impares[0]);
-
     return res;
 }
 
 int main(){
-    struct ContaNumeros teste = conta_numeros(10, 15);
+    struct ContaNumeros teste = conta_numeros(10, 21);
     printf("pares: %d\nimpares: %d\n", teste.pares, teste.impares);
 
-    printf("%d", teste.arr_pares[0]);
 
+    for(int i = 0; i < teste.distancia; i++){
+        printf("%d\n", teste.arr_impares[i]);
+    }
+
+    printf("-------------\n");
+
+    for(int i = 0; i < teste.distancia; i++){
+        printf("%d\n", teste.arr_pares[i]);
+    }
 
     return 0;
 }
--- tabela.md
+++ tabela.md
@@ -5,4 +5,4 @@ ex1: OK
 ex2: OK
 ex4: OK
 ex6: OK
-
+ex7: OK (mas dava pra melhorar)
