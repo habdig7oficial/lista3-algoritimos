fd1b703f25a407f54f939e1992b7d0db097bd392
Mateus Vieira <habdig7@gmail.com>
2025-03-10 13:06:16 -0300

    ex2 concluido

--- ex2.c
+++ ex2.c
@@ -2,9 +2,22 @@
 #include "stdbool.h"
 
 bool is_prime(int n){
-    for(j)
+
+    if(n <= 1)
+        printf("Não são primos por definição\n");
+
+    for(int i = 2; i <= n / 2; i++){
+        //printf("%d - %d\n", i, n % i == 0);
+        if(n % i == 0)
+            return false;
+    }
+    return true;
 }
 
 int main(){
-
+    for(int i = 0; i < 100; i++){
+        if(is_prime(i))
+            printf("%d\n",i);
+    }
+    return 0;
 }
