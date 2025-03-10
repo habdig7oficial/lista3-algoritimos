7e7c545ea11ac2896dead2a094da455fe3d0dc58
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-03-10 14:49:09 -0300

    ok

--- ex7.c
+++ ex7.c
@@ -1,3 +1,26 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
 
+struct ContaNumeros{
+    int pares;
+    int impares;
+    int *arr_pares;
+    int *arr_impares;
+};
 
+struct ContaNumeros conta_numeros(int inicio, int fim){
+    int intervalo = fim - inicio;
+
+    struct ContaNumeros res = {
+        .pares = floor(intervalo / 2)
+    };
+
+    return res;
+}
+
+int main(){
+    struct ContaNumeros teste = conta_numeros(10, 20);
+    printf("%d", teste.pares);
+    return 0;
+}
