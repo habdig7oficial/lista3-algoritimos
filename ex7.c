de6c5adc34b335acfd26e06a465a2bb84f91e354
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-03-10 15:12:56 -0300

    só pra garantir

--- ex7.c
+++ ex7.c
@@ -12,15 +12,43 @@ struct ContaNumeros{
 struct ContaNumeros conta_numeros(int inicio, int fim){
     int intervalo = fim - inicio;
 
+    int med = floor(intervalo / 2);
+    int num_impares[med];
+    int num_pares[med];
     struct ContaNumeros res = {
-        .pares = floor(intervalo / 2)
+        .pares = med,
+        .impares = med,
+        .arr_pares = num_pares,
+        .arr_impares = num_impares
     };
+
+
+
+    int index_par = 0;
+    int index_impar = 0;
+    /* Mais um pra pular o inicio fora do intervalo */
+    for (int i = inicio + 1; i < fim; i++){
+        if(i % 2 == 0){
+            num_pares[index_par] = i;
+            index_par++;
+        } 
+        else {
+            num_impares[index_impar] = i;
+            index_impar++;
+        }
+    }
+
+    printf("%d\n\n",res.arr_impares[0]);
 
     return res;
 }
 
 int main(){
-    struct ContaNumeros teste = conta_numeros(10, 20);
-    printf("%d", teste.pares);
+    struct ContaNumeros teste = conta_numeros(10, 15);
+    printf("pares: %d\nimpares: %d\n", teste.pares, teste.impares);
+
+    printf("%d", teste.arr_pares[0]);
+
+
     return 0;
 }
